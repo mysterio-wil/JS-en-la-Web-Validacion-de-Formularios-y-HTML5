3d503eb7d3ccf8d5125a3e120155808b058cd2d0
Wilmer Gulcochia <karlwgs1989@gmail.com>
2023-03-08 14:43:14 -0500

    Clase 04: Mensajes customizados - mostrando mensajes de error

--- js/validaciones.js
+++ js/validaciones.js
@@ -3,6 +3,31 @@ export function valida(input) {
   if (validadores[tipoDeInput]) {
     validadores[tipoDeInput](input);
   }
+  /* console.log(input.parentElement); */
+  if(input.validity.valid) {
+    input.parentElement.classList.remove("input-container--invalid");
+
+  } else {
+    input.parentElement.classList.add("input-container--invalid");
+  }
+}
+
+const mensajesDeError = {
+  nombre: {
+    valueMissing: "Este campo no puede estar vacío"
+  },
+  email: {
+    valueMissing: "Este campo no puede estar vacío",
+    typeMismatch: "El correo no es válido"
+  },
+  password: {
+    valueMissing: "Este campo no puede estar vacío",
+    patternMismatch: "Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales"
+  },
+  nacimiento: {
+    valueMissing: "Este campo no puede estar vacío",
+    customErro: "Debes tener al menos 18 años de edad"
+  }
 }
 
 const validadores = {
